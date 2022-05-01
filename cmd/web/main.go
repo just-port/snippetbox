9e5dec2c3a67b74ed0cb7643eb5d2ae262849432
Justin Portell <72322829+just-port@users.noreply.github.com>
2022-05-01 15:07:16 -0500

    WIP

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,9 +4,13 @@ import (
   "flag"
   "log"
   "net/http"
-  "path/filepath"
   "os"
 )
+
+type application struct {
+  errorLog *log.Logger
+  infoLog *log.Logger
+}
 
 func main() {
   addr := flag.String("addr", ":4000", "HTTP network address")
@@ -14,51 +18,29 @@ func main() {
   flag.Parse()
 
   infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
   errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+  app := &application{
+    errorLog: errorLog,
+    infoLog: infoLog,
+  }
 
   mux := http.NewServeMux()
 
-  fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+  fileServer := http.FileServer(http.Dir("./ui/static/"))
   mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+  mux.HandleFunc("/", app.home)
+  mux.HandleFunc("/snippet/view", app.snippetView)
+  mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-  srv := &http.Serve{
+  srv := &http.Server{
     Addr: *addr,
     ErrorLog: errorLog,
     Handler: mux,
   }
 
   infoLog.Printf("Starting server on %s", *addr)
-  err := srv.ListenAndServe(*addr, mux)
+  err := srv.ListenAndServe()
   errorLog.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-  fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-  f, err := nfs.fs.Open(path)
-  if err != nil {
-    return nil, err
-  }
-
-  s, err := f.Stat()
-  if s.IsDir() {
-    index := filepath.Join(path, "index.html")
-    if _, err := nfs.fs.Open(index); err != nil {
-      closeErr := f.Close()
-      if closeErr != nil {
-        return nil, closeErr
-      }
-
-      return nil, err
-    }
-  }
-
-  return f, nil
 }
